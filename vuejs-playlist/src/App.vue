1f898aac3393938f996a1ae9cfbbf23cf265ae98
Uroš <uros.gomilsek@metos.at>
2018-08-13 11:14:08 +0200

    first commit

---
+++ vuejs-playlist/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <form-helper>
+    <div slot="form-header">
+
+    </div>
+    </form-helper>
+  </div>
+
+
+
+</template>
+
+<script>
+import formHelper from './components/formHelper.vue'
+export default {
+  components:{
+    'form-helper': formHelper
+  },
+  data() {
+    return {
+    title: 'Dynamic slot title'
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+h2 {
+  color:red;
+}
+</style>
---
+++ vuejs-playlist/src/components/header.vue
@@ -0,0 +1,39 @@
+<template>
+<header>
+<h1 @click="changeTitle"> {{title}}  </h1>
+</header>
+
+</template>
+
+<script>
+import { bus } from '../main';
+
+export default {
+  data() {
+    return {
+        title: 'Ninja app'
+    }
+  },
+  methods: {
+    changeTitle: function() {
+      //this.$emit('changeTitle', 'Vue Wizards');
+      this.title = 'Vue Avtobus this.title';
+      bus.$emit('titleChanged', 'Vue Avtobus');
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+header {
+  background: red;
+  padding: 10px;
+}
+
+h1{
+  color: #222;
+  text-align: center;
+}
+
+</style>
